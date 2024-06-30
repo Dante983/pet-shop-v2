7d90a80fa1278d7d0e3354bc9f024743b0ca13db
Nikola Savic <savicn209@gmail.com>
2024-06-30 02:49:02 +0200

    added user controller and create function

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 
 Route::prefix('v1')->group(function () {
     Route::prefix('admin')->group(function () {
         Route::post('create', [AdminController::class, 'create']);
+    });
+
+    Route::prefix('user')->group(function () {
+        Route::post('create', [UserController::class, 'create']);
     });
 });

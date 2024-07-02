e8c1cef30d6f982e9b874a1a1c7d5baee64400d6
Nikola Savic <savicn209@gmail.com>
2024-07-02 20:19:48 +0200

    created migration for categories brands products and jwt tokens tables

---
+++ database/migrations/2024_07_02_181214_create_category_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->unique()->default(Str::uuid());
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};

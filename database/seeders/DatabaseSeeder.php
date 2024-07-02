ec7646dc4ec3147124fe78b76e0095e04a05a27b
Nikola Savic <savicn209@gmail.com>
2024-07-02 22:15:12 +0200

    added factories to seed the db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,15 @@
 
 namespace Database\Seeders;
 
+use App\Models\Brand;
+use App\Models\Categories;
+use App\Models\Post;
+use App\Models\Promotion;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -23,11 +27,16 @@ class DatabaseSeeder extends Seeder
                 'first_name' => 'Admin',
                 'last_name' => 'User',
                 'email' => 'admin@buckhill.co.uk',
-                'password' => Hash::make('admin'), // Replace with a secure password
+                'password' => Hash::make('admin'),  // Replace with a secure password
                 'is_admin' => true,
                 'address' => 'Admin Address',
                 'phone_number' => '1234567890',
             ]
         );
+
+        Categories::factory()->count(5)->create();
+        Brand::factory()->count(5)->create();
+        Post::factory()->count(10)->create();
+        Promotion::factory()->count(3)->create();
     }
 }
